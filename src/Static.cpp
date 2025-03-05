5ffd0e464c139b18eeac44639766cd98936867af
Ohmwrecker <2512008088@qq.com>
2025-03-05 21:42:56 +0800

    移动Log.h

--- src/Static.cpp
+++ src/Static.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+// 单例模式
+// 保证一个类只有一个实例，并提供一个访问它的全局访问点
+class Singleton {
+public:
+	static Singleton& Get() {
+		static Singleton instance;
+		return instance;
+	}
+	void Print() {}
+};
+
 // 当使用静态声明i，重复5次Function()函数，输出结果为5
 void Static_i_Function() {
 	static int i = 0;
@@ -12,5 +23,5 @@ void NonStatic_i_Function() {
 	int i = 0;
 	i++;
 	std::cout << i << std::endl;
-}
+} 
 // 如果想让方法内的变量不在每次调用的时候初始化，又不想让方法外访问这个变量，就使用静态声明。
