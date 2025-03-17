41fbdcf4b1cf89ae2ef7dab7a2a367fab8184fb8
Ohmwrecker <2512008088@qq.com>
2025-03-17 21:17:03 +0800

    GLFW依赖添加

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,6 +11,8 @@
 #include "CopyConstructor.cpp"
 #include "Arrow_Operator.cpp"
 #include "Vector.cpp"
+#include "GLFW/glfw3.h"
+
 
 int main() {
 
@@ -51,7 +53,7 @@ int main() {
         //NonStatic_i_Function();
     }
 
-    // Log
+    //// Log
     //Log log(Log::LogLevel::LogLevel_Info); 
     // 使用::（类名和作用域解析符）对Log命名空间内的LogLevel进行访问
     //log.Info("Hello, world!");
@@ -60,8 +62,8 @@ int main() {
     //PrintLogLevel printLogLevel;
     //printLogLevel.Print();
 
-    // VirtualFunction
-    //使用虚函数对基类和派生类进行测试
+    //// VirtualFunction
+    //// 使用虚函数对基类和派生类进行测试
     //Entity* e = new Entity();
     //std::cout << "Entity::name = " << e->getName() << std::endl;
 
@@ -76,20 +78,20 @@ int main() {
     //delete p;
     //PrintClassName(a);
     //delete a;
-    // 直接使用new a()会导致内存泄漏，最好显式创建指针变量并删除
+    //// 直接使用new a()会导致内存泄漏，最好显式创建指针变量并删除
 
-    // Array
+    //// Array
     //Array* array = new Array();
     //array->changeValue();
     //std::cin.get();
 
-    // String
+    //// String
     //String* str = new String();
     //std::cout << "String::str = " << str->name << std::endl;
     //std::cout << "String::str = " << str->name2 << std::endl;
 
 
-    // Operator
+    //// Operator
     //Vector2 v1(1.0f, 2.0f);
     //Vector2 v2(3.0f, 4.0f);
     //Vector2 result1 = v1 + v2;
@@ -97,14 +99,14 @@ int main() {
     //std::cout << "result1: " << result1.x << ", " << result1.y << std::endl;
     //std::cout << "result2: " << result2.x << ", " << result2.y << std::endl;
 
-    // 直接将Vector2传入ostream不可行，需要重载<<运算符
+    //// 直接将Vector2传入ostream不可行，需要重载<<运算符
     //std::cout << result1 << std::endl;
     //std::cout << result2 << std::endl;
 
-    // This
+    //// This
     //This_Operator(114514,1919810);
 
-    //Scope
+    ////Scope
     //{
     //    // 使用作用域指针，让即使是堆上分配的对象也会在作用域结束时自动释放
     //    Scope e = new ScopeEntity();
@@ -120,8 +122,8 @@ int main() {
     //}
     //This_Operator(114514,1919810);
 
-    // Arrow_Operator
-    // 使用箭头操作符计算偏移量
+    //// Arrow_Operator
+    //// 使用箭头操作符计算偏移量
     //int offset = (int)&((Vector3*)0)->z;
     //std::cout << "offset = " << offset << std::endl;
     //{
@@ -129,41 +131,44 @@ int main() {
     //    ScopePtr_Vector3_test->x = 1.0f;
     //}
 
-    // Vector
-    // 实际就是ArrayList
-    // 静态分配数组
+    //// Vector
+    //// 实际就是ArrayList
+    //// 静态分配数组
     //Vertex* vertex = new Vertex[10];
-    // 初始化数组
+    //// 初始化数组
     //vertex[0] = { 1.0f, 2.0f, 3.0f };
     //delete[] vertex;
-    // 使用std::vector动态分配数组
-    // 使用Vertex或Vertex*，前者为顺序存储，后者为随机存储
-    // std::vector<Vertex> vertex(10)是创建了10个Vertex对象
-    std::vector<Vertex> vertex2;
-    // 使用reserve预分配内存减少复制优化性能
-    vertex2.reserve(4);
-    // 初始化数组，注意push_back和emplace_back的区别，前者在main函数栈内创建对象复制到Vertex内存空间，后者直接在Vertex内存空间构造对象
-    vertex2.push_back(Vertex(1.0f, 2.0f, 3.0f));
-    vertex2.push_back(Vertex(4.0f, 5.0f, 6.0f));
-    //使用emplace_back直接构造对象放入Vertex内存空间，直接避免复制
-    vertex2.emplace_back(7.0f, 8.0f, 9.0f);
-    vertex2.emplace_back(10.0f, 11.0f, 12.0f);
-    // 增强for循环，使用&避免复制
-    for (auto& v : vertex2) {
-        std::cout << v << std::endl;
-    }
-    // 释放内存
-    //vertex2.clear();
-    // 也可以单独删除元素
-    // begin()为数组第一个元素的指针
-    vertex2.erase(vertex2.begin() + 1);
+    //// 使用std::vector动态分配数组
+    //// 使用Vertex或Vertex*，前者为顺序存储，后者为随机存储
+    //// std::vector<Vertex> vertex(10)是创建了10个Vertex对象
+    //std::vector<Vertex> vertex2;
+    //// 使用reserve预分配内存减少复制优化性能
+    //vertex2.reserve(4);
+    //// 初始化数组，注意push_back和emplace_back的区别，前者在main函数栈内创建对象复制到Vertex内存空间，后者直接在Vertex内存空间构造对象
+    //vertex2.push_back(Vertex(1.0f, 2.0f, 3.0f));
+    //vertex2.push_back(Vertex(4.0f, 5.0f, 6.0f));
+    ////使用emplace_back直接构造对象放入Vertex内存空间，直接避免复制
+    //vertex2.emplace_back(7.0f, 8.0f, 9.0f);
+    //vertex2.emplace_back(10.0f, 11.0f, 12.0f);
+    //// 增强for循环，使用&避免复制
+    //for (auto& v : vertex2) {
+    //    std::cout << v << std::endl;
+    //}
+    //// 释放内存
+    ////vertex2.clear();
+    //// 也可以单独删除元素
+    //// begin()为数组第一个元素的指针
+    //vertex2.erase(vertex2.begin() + 1);
 
-    // CopyConstructor
-    CopyConstructor string = "Ohmwrecker";
-    CopyConstructor string2 = string;
-    string2[0] = 'X';
-    std::cout << "string = " << string <<std::endl;
-    std::cout << "string2 = " << string2 << std::endl;
+    //// CopyConstructor
+    //CopyConstructor string = "Ohmwrecker";
+    //CopyConstructor string2 = string;
+    //string2[0] = 'X';
+    //std::cout << "string = " << string <<std::endl;
+    //std::cout << "string2 = " << string2 << std::endl;
+
+    int a = glfwInit();
+    std::cout << a << std::endl;
 
     return 0;
 }
