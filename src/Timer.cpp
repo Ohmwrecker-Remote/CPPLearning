734dad0c9edd834c48d98ba38d6aea4cb57598ad
Ohmwrecker <2512008088@qq.com>
2025-03-24 18:08:10 +0800

    Timer

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -6,12 +6,16 @@
 class Timer {
 private:
 	// 使用chrono的高精度时钟获取当前时间
-	std::chrono::steady_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+	std::chrono::time_point<std::chrono::steady_clock> start_time, end_time;
+	std::chrono::duration<float> duration = std::chrono::duration<float>::zero();
 public:
+	Timer() {
+		this->start_time = std::chrono::high_resolution_clock::now();
+	};
 	~Timer() {
 		// 计算并输出时间
-		auto end_time = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration<double>(end_time - this->start_time).count();
-		std::cout << "Time taken: " << duration << " microseconds" << std::endl;
+		this->end_time = std::chrono::high_resolution_clock::now();
+		this->duration = this->end_time - this->start_time;
+		std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::milliseconds>(this->duration).count() << " 毫秒" << std::endl;
 	};
 };
